c7bafca52d98e4caee8c6cd0d5d5734d5d8fe953
George Voutyras <geovoutyras@gmail.com>
2022-01-09 20:48:04 +0200

    feat: environment config

---
+++ src/middlewares/apikey.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import minimist from "minimist";
+
+export default function apikey(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Response | void {
+  const argv = minimist(process.argv.slice(2));
+  const useApiKey: string = argv["api-key"];
+
+  if (useApiKey || useApiKey === undefined) {
+    const apiKey = req.header("x-api-key");
+
+    if (apiKey === process.env.FIXED_API_KEY) {
+      return next();
+    } else {
+      return res
+        .status(403)
+        .json({ status: false, code: 403, message: "Invalid API key" });
+    }
+  }
+  return next();
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// The base directory that we want to use
+const baseDirectory = "dist";
+
+module.exports = {
+  // The current mode, defaults to production
+  mode: "development",
+
+  // The entry points ("location to store": "location to find")
+  entry: {
+    "public/js/scripts": [`./${baseDirectory}/public/ts/scripts`],
+    // "other output points" : ["other entry point"]
+  },
+
+  // Used for generating source maps (used for debugging)
+  devtool: "eval-source-map",
+
+  // The location where bundle are stored
+  output: {
+    filename: "[name].js",
+  },
+};
